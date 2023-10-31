4f13a753d6287759ab459581f6b0c1ffed7eb719
Staaalin <943319330@qq.com>
2023-10-31 18:48:11 +0800

    Update readTree.C

--- RootFile/readTree.C
+++ RootFile/readTree.C
@@ -1,111 +1,134 @@
-#include <TSystem>
-//#include "RooFit.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <random>
+#include "math.h"
+#include "string.h"
+#include <vector>
+#ifndef __CINT__
+#include "TROOT.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TChain.h"
+#include "TF1.h"
+#include "TH1.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TTree.h"
+#include "TNtuple.h"
+#include "TRandom.h"
+#include "TMath.h"
+#include "TVector3.h"
+#include "TLorentzVector.h"
+#include "TSystem.h"
+#include "TLegend.h"
+#include "TUnixSystem.h"
+#include "TRandom3.h"
+#endif
+#include <iostream>
+#include <map>
+#include <stdio.h>
+using namespace std;
 
-class StMaker;
-class StChain;
-class StPicoDstMaker;
-class StPicoEvent;
-class StRefMultCorr;
-class StEpdGeom;
-class StEpdEpInfo;
-class StEpdEpFinder;
-
-
-//StChain *chain;
-
-void readPicoDst(const Char_t *inputFile="test.list", int jobindex, int run=11, float energy=200.0, Char_t *ListDir="./datalist/")
+void readTree()
 {
-	int iJob = jobindex;
-	cout << "current job id: " << iJob << endl;
+    const Int_t maxMultiplicity = 20000;
+    Int_t refMult,grefMult;
+    Int_t PDG[maxMultiplicity],evtID[maxMultiplicity],runID[maxMultiplicity];
+    Float_t px[maxMultiplicity],py[maxMultiplicity],pz[maxMultiplicity];
+    Float_t mass[maxMultiplicity],energy[maxMultiplicity];
 
-	TString StrOutName = Form("output_%.6d.root", iJob);
-	const Char_t *outputFile = StrOutName.Data();
-
-	Long64_t nEvents = 10000000000;
-	// nEvents = 300;
-
-	//Load all the System libraries
-	gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
-	loadSharedLibraries();
-
- 	gROOT->LoadMacro("lMuDst.C");
-	TString outputKFParticleQA = Form("KFParticleQA_%.6d.root", iJob);
-	lMuDst(-1,inputFile,"ry2016,RpicoDst,kfpInter,mysql,quiet,nodefault",outputKFParticleQA.Data());
-
-	//gSystem->AddIncludePath("-lRooFitCore");
-	//gSystem->Load("libRooFit");
-	gSystem->Load("StUtilities");
-	gSystem->Load("StRefMultCorr");
-	gSystem->Load("StPicoEvent");
-	gSystem->Load("StPicoDstMaker");
-	//gSystem->Load("StEpdUtil");
-	gSystem->Load("StEpdUtil.so");
-	//gSystem->Load("StLambdaDecayPair");
-	//gSystem->Load("StKFParticleAnalysisMaker");
-	//gSystem->Load("KFParticle");
-
-	StKFParticleAnalysisMaker *anaMaker = new StKFParticleAnalysisMaker("ana", outputFile);
-	anaMaker->setRunEnergyAndListDir(run,energy,ListDir);            
-
-	// Loop over the links in the chain
-	Int_t iInit = chain -> Init() ;
-	if (iInit) chain->Fatal(iInit,"on init");
-	cout<<"chain->Init();"<<endl;
-
-	// KFParticle
-	StKFParticleInterface::instance()->CleanLowPVTrackEvents();
-	//StKFParticleInterface::instance()->UseHFTTracksOnly();
-	StKFParticleInterface::instance()->SetSoftKaonPIDMode();
-	StKFParticleInterface::instance()->SetSoftTofPidMode();
-	StKFParticleInterface::instance()->SetChiPrimaryCut(10);
-	//Add decays to the reconstruction list
-	StKFParticleInterface::instance()->AddDecayToReconstructionList( 3122);
-	StKFParticleInterface::instance()->AddDecayToReconstructionList(-3122);
-	StKFParticleInterface::instance()->AddDecayToReconstructionList( 3334);
-
-	// StPicoDstMaker & chain
-	StPicoDstMaker* maker = (StPicoDstMaker *) StMaker::GetTopChain()->Maker("PicoDst");
-	if (! maker) return;
-	maker->SetStatus("*",1);
-	TChain *tree = maker->chain();
-	Long64_t nentries = tree->GetEntries();
-	if (nentries <= 0) return;
-	Long64_t EventsToRun = TMath::Min(nEvents,nentries);
-	cout << nentries << " events in chain " << EventsToRun << " will be read." << endl;
-
-	time_t time_start;
-	time_t time_now;
-	time(&time_start);
-
-	Int_t istat = 0, i = 1;
-	while(i<=EventsToRun && istat!=2) {
-		//if(i%5000==0){cout << endl; cout << "== Event " << i << " start ==" << endl;}
-		chain->Clear();
-		istat = chain->Make(i);
-		if(i%100==0) { // 100
-			cout << endl; 
-			cout << "== Event " << i << " finish == " << flush;
-			time(&time_now);
-			int time_diff = (int)difftime(time_now, time_start);
-			cout << time_diff/60 << "min " << time_diff%60 << "s: " << 1.0*time_diff/i << "s/event" << endl;
-		}
-		if (istat == 2)
-			cout << "Last  event processed. Status = " << istat << endl;
-		if (istat == 3)
-			cout << "Error event processed. Status = " << istat << endl;
-		i++;
+    Int_t kBinNum = 1000;
+    Float_t kmin = 0;
+    Float_t kmax = 10;
+	TString ParticleName[] = { "Lambda" , "Lambdab" , "Omega" };
+	int ParticlePDG[]      = {   3122   ,   -3122   ,   3334  };
+	int HSize = sizeof(ParticleName)/sizeof(ParticleName[0]);
+	TH1D *HMass[HSize];
+	for (int i=0;i<ParticleName;i++){
+		TString HistName1 = "HM";
+		TString HistName2 = "The Mass of ";
+		HistName1 += ParticleName[i];
+		HistName2 += ParticleName[i];
+		HMass[i] = new TH1D(HistName1, HistName2, kBinNum, kmin, kmax);
 	}
 
+    //load data  
+    TString midname = "/star/data01/pwg/svianping/output/output_";
 
-	cout << "****************************************** " << endl;
-	cout << "Work done... now its time to close up shop!"<< endl;
-	cout << "****************************************** " << endl;
-	chain->Finish();
-	cout << "****************************************** " << endl;
-	cout << "total number of events  " << EventsToRun << endl;
-	cout << "****************************************** " << endl;
+    TChain *hadronTree = new TChain("hadronTree");
+    for(int i=2892;i <= 2991;i++){
+        TString filename = midname;
+        filename+="00";
+        filename+=i;
+        filename+=".root";
+        hadronTree->Add(filename);
+        // cout<<filename<<endl;
+    }
+    
+    hadronTree->SetBranchAddress("refMult",&refMult);
+    hadronTree->SetBranchAddress("grefMult",&grefMult);
+    hadronTree->SetBranchAddress("PDG",&PDG);
+    hadronTree->SetBranchAddress("px",&px);
+    hadronTree->SetBranchAddress("py",&py);
+    hadronTree->SetBranchAddress("pz",&pz);
+    hadronTree->SetBranchAddress("mass",&InvarentMass);
 
-	delete chain;
+    const Int_t nentries=hadronTree->GetEntries();
+    cout << "file number: " << nentries << endl;
+    
+    //read data
+    for (int i=0;i<nentries;i++){
+        hadronTree->GetEntry(i);
+
+		for (int j=0;j<)
+
+    }
+
+    TFile *file = new TFile("CheckOutput.root", "RECREATE");
+
+    Int_t binNorm[2];
+    binNorm[0] = Ak->FindBin(3);
+    binNorm[1] = Ak->FindBin(4); 
+    Double_t factorN1 = 1.0*Bk->Integral(binNorm[0], binNorm[1]) / Ak->Integral(binNorm[0], binNorm[1]);
+    Ck->Divide(Ak,Bk,1.0,1.0/factorN1);
+    Ck->Sumw2();
+
+    binNorm[0] = Ay->FindBin(3);
+    binNorm[1] = Ay->FindBin(4); 
+    Double_t factorN2 = 1.0*By->Integral(binNorm[0], binNorm[1]) / Ay->Integral(binNorm[0], binNorm[1]);
+    Cy->Divide(Ay,By,1.0,1.0/factorN2);
+    Cy->Sumw2();
+
+    Ak->GetXaxis()->SetTitle("k^{*} [GeV]");
+    Ak->GetYaxis()->SetTitle("Counts");
+    Bk->GetXaxis()->SetTitle("k^{*} [GeV]");
+    Bk->GetYaxis()->SetTitle("Counts");
+    Ck->GetXaxis()->SetTitle("k^{*} [GeV]");
+    Ck->GetYaxis()->SetTitle("C(k^{*})");
+    Ck->SetStats(0);
+
+    Ay->GetXaxis()->SetTitle("#Delta y");
+    Ay->GetYaxis()->SetTitle("Counts");
+    By->GetXaxis()->SetTitle("#Delta y");
+    By->GetYaxis()->SetTitle("Counts");
+    Cy->GetXaxis()->SetTitle("#Delta y");
+    Cy->GetYaxis()->SetTitle("C(#Delta y)");
+    Cy->SetStats(0);
+
+    Ak->Write();
+    Bk->Write();
+    Ck->Write();
+    
+    Ay->Write();
+    By->Write();
+    Cy->Write();
+    MIX_Con->Write();
+    
 
 
+    file->Write();
+
+    return 0;
 }
