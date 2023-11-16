66b3121044e3a8266ddde8bb84630d98a2e8e6cd
Staaalin <943319330@qq.com>
2023-11-16 16:58:52 +0800

    Add PID.C

---
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/ProtonPID.h
@@ -0,0 +1,28 @@
+#ifndef ProtonPID_hh
+#define ProtonPID_hh
+
+class ProtonPID
+{
+private:
+    // PID parameter
+    float zTOF;
+    float nSigma;
+    float pT;
+
+    // cut parameters
+    static const float num_std;
+    static const float nSigma_mean_proton[9];
+    static const float nSigma_std_proton[9];
+    static const float zTOF_mean_proton[9];
+    static const float zTOF_std_proton[9];
+    static const float nSigma_mean_antiproton[9];
+    static const float nSigma_std_antiproton[9];
+    static const float zTOF_mean_antiproton[9];
+    static const float zTOF_std_antiproton[9];
+    
+public:
+    ProtonPID(float _zTOF, float _nSigma, float _pT): zTOF(_zTOF), nSigma(_nSigma), pT(_pT) {}
+    // bool IsProton();
+    bool IsProtonSimple(float nSigmaCut, int charge);
+};
+#endif
