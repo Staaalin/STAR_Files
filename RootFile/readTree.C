5d4ba11329a9815c06871354d9985e2af07f6a8c
Staaalin <943319330@qq.com>
2023-11-28 11:38:25 +0800

    Update readTree.C

--- RootFile/readTree.C
+++ RootFile/readTree.C
@@ -55,14 +55,14 @@ void readTree()
         std::vector<Float_t> *dEdx            = nullptr;
         std::vector<Float_t> *m2              = nullptr;
         std::vector<Float_t> *dcatopv         = nullptr;
-        std::vector<Float_t> *hasTOF          = nullptr;
+        std::vector<int>     *hasTOF          = nullptr;
         std::vector<Float_t> *nSigmaProton    = nullptr;
         std::vector<Float_t> *nSigmaPion      = nullptr;
         std::vector<Float_t> *nSigmaKaon      = nullptr;
         std::vector<double>  *zTOF_proton     = nullptr;
         std::vector<double>  *zTOF_pion       = nullptr;
         std::vector<double>  *zTOF_kaon       = nullptr;
-	    std::vector<Float_t> *IfConfuse       = nullptr;
+	    std::vector<int>     *IfConfuse       = nullptr;
 
         TBranch *bdEdx            = nullptr;
         TBranch *bm2              = nullptr;
@@ -99,14 +99,14 @@ void readTree()
             std::vector<Float_t> *dEdx            = NULL;
             std::vector<Float_t> *m2              = NULL;
             std::vector<Float_t> *dcatopv         = NULL;
-            std::vector<Float_t> *hasTOF          = NULL;
+            std::vector<int>     *hasTOF          = NULL;
             std::vector<Float_t> *nSigmaProton    = NULL;
             std::vector<Float_t> *nSigmaPion      = NULL;
             std::vector<Float_t> *nSigmaKaon      = NULL;
             std::vector<double>  *zTOF_proton     = NULL;
             std::vector<double>  *zTOF_pion       = NULL;
             std::vector<double>  *zTOF_kaon       = NULL;
-            std::vector<Float_t> *IfConfuse       = NULL;
+            std::vector<int>     *IfConfuse       = NULL;
 
             TBranch *bdEdx            = NULL;
             TBranch *bm2              = NULL;
@@ -142,14 +142,14 @@ void readTree()
             std::vector<Float_t> *dEdx            = 0;
             std::vector<Float_t> *m2              = 0;
             std::vector<Float_t> *dcatopv         = 0;
-            std::vector<Float_t> *hasTOF          = 0;
+            std::vector<int>     *hasTOF          = 0;
             std::vector<Float_t> *nSigmaProton    = 0;
             std::vector<Float_t> *nSigmaPion      = 0;
             std::vector<Float_t> *nSigmaKaon      = 0;
             std::vector<double>  *zTOF_proton     = 0;
             std::vector<double>  *zTOF_pion       = 0;
             std::vector<double>  *zTOF_kaon       = 0;
-            std::vector<Float_t> *IfConfuse       = 0;
+            std::vector<int>     *IfConfuse       = 0;
 
             TBranch *bdEdx            = 0;
             TBranch *bm2              = 0;
@@ -170,14 +170,18 @@ void readTree()
     Int_t kBinNum = 1000;
     Float_t kmin = 0;
     Float_t kmax = 10;
-	const TString ParticleName[] = { "Lambda" , "Lambdab" , "Omega"  , "Proton"  , "Protonb" , "Pion" , "Pionb"  , "Kaon" , "Kaonb" };
-	const int ParticlePDG[]      = {   3122   ,   -3122   ,   3334   ,   2212    ,  -2212    ,  211   ,  -211    ,  321   ,  -321   };
+	const TString ParticleName[] = { "Lambda" , "Lambdab" , "Omega"  , "Proton"  , "Protonb" , "Pion" , "Pionb"  , "Kaon" , "Kaonb" , "Un-Identified"};
+	const int ParticlePDG[]      = {   3122   ,   -3122   ,   3334   ,   2212    ,  -2212    ,  211   ,  -211    ,  321   ,  -321   ,       0        };
 	// const int HSize = sizeof(ParticleName)/sizeof(ParticleName[0]);
-	const int HSize = 9;
+	const int HSize = 10;
 	TH1D *HMass[HSize];
 	TH1D *HP[HSize];
 	TH1D *HRapidity[HSize];
+	TH1D *Hdcatopv[HSize];
 	TH2D *H_dEdx_p[HSize];
+	TH2D *H_sigma_TOF[HSize];
+    // for confused
+	TH2D *HC_sigma_TOF[3];
 	for (int i=0;i<HSize;i++){
 		TString HistName1 = "HM";
 		TString HistName2 = "The Mass of ";
@@ -203,14 +207,48 @@ void readTree()
 		HRapidity[i]->GetXaxis()->SetTitle("y");
 		HRapidity[i]->GetYaxis()->SetTitle("Counts");
         
+		TString HistName1 = "Hdcatopv";
+		TString HistName2 = "The dcatopv of ";
+		HistName1 += ParticleName[i];
+		HistName2 += ParticleName[i];
+		HRapidity[i] = new TH1D(HistName1, HistName2, 30, -1.5, 1.5);
+		HRapidity[i]->GetXaxis()->SetTitle("DCA to PV [cm]");
+		HRapidity[i]->GetYaxis()->SetTitle("Counts");
+
 		TString HistName1 = "H_dEdx_p";
 		TString HistName2 = "The dE/dx vs. p of ";
 		HistName1 += ParticleName[i];
 		HistName2 += ParticleName[i];
-		HRapidity[i] = new TH2D(HistName1, HistName2, kBinNum/10, kmin, kmax, 100, 0, 15);
-		HRapidity[i]->GetXaxis()->SetTitle("p [GeV]");
-		HRapidity[i]->GetYaxis()->SetTitle("dE/dx [keV/cm]");
+		H_dEdx_p[i] = new TH2D(HistName1, HistName2, kBinNum, kmin, kmax, 100, 0, 15);
+		H_dEdx_p[i]->GetXaxis()->SetTitle("p [GeV]");
+		H_dEdx_p[i]->GetYaxis()->SetTitle("dE/dx [keV/cm]");
+        
+		TString HistName1 = "H_sigma_TOF";
+		HistName1 += ParticleName[i];
+		TString HistName2 = "The nSigma";
+		HistName2 += ParticleName[i];
+        HistName2 += " vs. zTOF";
+		HistName2 += ParticleName[i];
+        HistName2 += " of ";
+		HistName2 += ParticleName[i];
+		H_sigma_TOF[i] = new TH2D(HistName1, HistName2, 200, -35, 35, 200, -4, 4);
+		H_sigma_TOF[i]->GetXaxis()->SetTitle("nSigma");
+		H_sigma_TOF[i]->GetYaxis()->SetTitle("zTOF");
 	}
+	const TString ConfusedParticleName[] = {"Proton" , "Pion"  , "Kaon"};
+    for (int i=0;i<3;i++) {
+		TString HistName1 = "HC_sigma_TOF";
+		HistName1 += ConfusedParticleName[i];
+		TString HistName2 = "The nSigma";
+		HistName2 += ConfusedParticleName[i];
+        HistName2 += " vs. zTOF";
+		HistName2 += ConfusedParticleName[i];
+        HistName2 += " of ";
+		HistName2 += ConfusedParticleName[i];
+		HC_sigma_TOF[i] = new TH2D(HistName1, HistName2, 200, -35, 35, 200, -4, 4);
+		HC_sigma_TOF[i]->GetXaxis()->SetTitle("nSigma");
+		HC_sigma_TOF[i]->GetYaxis()->SetTitle("zTOF");
+    }
 
     //load data  
     TChain *hadronTree = new TChain("hadronTree");
@@ -289,16 +327,55 @@ void readTree()
         }
 		for (int j=0;j<PDGMult;j++){
 			for (int k=0;k<HSize;k++){
-				if(PDG->at(j) == ParticlePDG[k]){
+				if (PDG->at(j) == ParticlePDG[k] && IfConfuse->at(j) != 1){
                     TLorentzVector p0;
+                    Float_t Mag = pow(pow(px->at(j),2) + pow(py->at(j),2) + pow(pz->at(j),2),0.5);
                     p0.SetPxPyPzE(px->at(j),py->at(j),pz->at(j),pow(pow(px->at(j),2) + pow(py->at(j),2) + pow(pz->at(j),2) + pow(InvariantMass->at(j),2),0.5));
                     float rap = p0.Rapidity();
 
 					HMass[k]->Fill(InvariantMass->at(j));
-					HP[k]->Fill(pow(pow(px->at(j),2) + pow(py->at(j),2) + pow(pz->at(j),2),0.5));
+					HP[k]->Fill(Mag);
 					HRapidity[k]->Fill(rap);
+                    if (fabs(PDG->at(j)) == 2212){
+                        Hdcatopv[k]->Fill(dcatopv->at(j));
+                        H_dEdx_p[k]->Fill(Mag,dEdx->at(j));
+                        H_sigma_TOF[k]->Fill(nSigmaProton->at(j),zTOF_proton->at(j));
+                    }
+                    if (fabs(PDG->at(j)) == 211){
+                        Hdcatopv[k]->Fill(dcatopv->at(j));
+                        H_dEdx_p[k]->Fill(Mag,dEdx->at(j));
+                        H_sigma_TOF[k]->Fill(nSigmaPion->at(j),zTOF_pion->at(j));
+                    }
+                    if (fabs(PDG->at(j)) == 321){
+                        Hdcatopv[k]->Fill(dcatopv->at(j));
+                        H_dEdx_p[k]->Fill(Mag,dEdx->at(j));
+                        H_sigma_TOF[k]->Fill(nSigmaKaon->at(j),zTOF_kaon->at(j));
+                    }
 					break;
 				}
+
+                if (IfConfuse->at(j) == 1) {
+                    TLorentzVector p0;
+                    Float_t Mag = pow(pow(px->at(j),2) + pow(py->at(j),2) + pow(pz->at(j),2),0.5);
+                    p0.SetPxPyPzE(px->at(j),py->at(j),pz->at(j),pow(pow(px->at(j),2) + pow(py->at(j),2) + pow(pz->at(j),2) + pow(InvariantMass->at(j),2),0.5));
+                    float rap = p0.Rapidity();
+
+					HMass[HSize-1]->Fill(InvariantMass->at(j));
+					HP[HSize-1]->Fill(Mag);
+					HRapidity[HSize-1]->Fill(rap);
+                    Hdcatopv[HSize-1]->Fill(dcatopv->at(j));
+                    H_dEdx_p[HSize-1]->Fill(Mag,dEdx->at(j));
+                    if (fabs(PDG->at(j)) == 2212){
+                        HC_sigma_TOF[0]->Fill(nSigmaProton->at(j),zTOF_proton->at(j));
+                    }
+                    if (fabs(PDG->at(j)) == 211){
+                        HC_sigma_TOF[1]->Fill(nSigmaPion->at(j),zTOF_pion->at(j));
+                    }
+                    if (fabs(PDG->at(j)) == 321){
+                        HC_sigma_TOF[2]->Fill(nSigmaKaon->at(j),zTOF_kaon->at(j));
+                    }
+					break;
+                }
 			}
 		}
 
@@ -327,6 +404,12 @@ void readTree()
 		HMass[i]->Write();
 		HP[i]->Write();
 		HRapidity[i]->Write();
+	    Hdcatopv->Write();
+	    H_dEdx_p->Write();
+	    H_sigma_TOF->Write();
+	}
+	for (int i=0;i<3;i++){
+        HC_sigma_TOF[i]->Write();
 	}
 
     file->Write();
