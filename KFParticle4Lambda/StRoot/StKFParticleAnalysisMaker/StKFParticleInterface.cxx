3dc92f4a17e5fb69e5bc3bdb37f33d8162903fda
Staaalin <943319330@qq.com>
2023-12-19 14:40:44 +0800

    Update StKFParticleInterface.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleInterface.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleInterface.cxx
@@ -718,7 +718,7 @@ bool StKFParticleInterface::ProcessEvent(StPicoDst* picoDst, std::vector<int>& t
   fParticlesPdg.resize(nPartSaved);
   fNHftHits.resize(nPartSaved);
   
-  if( fCleanLowPVTrackEvents && ( 10*primaryTrackList.size() < (nUsedTracks - primaryTrackList.size()) ) ) return 0;
+  // if( fCleanLowPVTrackEvents && ( 10*primaryTrackList.size() < (nUsedTracks - primaryTrackList.size()) ) ) return 0;
   
   const Double_t field = picoEvent->bField();  
   SetField(field);
