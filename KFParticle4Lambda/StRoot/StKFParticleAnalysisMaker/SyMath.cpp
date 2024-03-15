179049fdca16cfd2299bb6247c86c9032596488c
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-03-15 23:56:40 +0800

    Update SyMath.cpp

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/SyMath.cpp
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/SyMath.cpp
@@ -6,29 +6,26 @@
 float NMin(float (*func)(float),
            float Input_X_min,
            float Input_X_max,
-           char  Method,
-           int MAX_Itr)
+           int   MAX_Itr)
     {
     
     float X_min = 1.0*Input_X_min;
     float X_max = 1.0*Input_X_max;
-    if (Method == 'BI' || Method == 'BISECTION'){
-        float X_mid,Y_mid;
-        float Y_Left  = (*func)(X_min);
-        float Y_Right = (*func)(X_max);
-        for(int Itr=1;Itr <= MAX_Itr;Itr++){
-            Y_Left  = (*func)(X_min);
-            Y_Right = (*func)(X_max);
+    float X_mid = 0.5*(X_min + X_max);
+    float X_Tem;
+    float Y_Left,Y_Right,Y_Mid;
+    Y_Left  = (*func)(X_min);
+    Y_Mid   = (*func)(X_mid);
+    Y_Right = (*func)(X_max);
+    for(int Itr=1;Itr <= MAX_Itr;Itr++){
 
-            if (Y_Left * Y_Right < 0){
-                X_mid = 0.5*(X_min + X_max);
-                Y_mid = (*func)(X_mid);
-                if(Y_Left * Y_mid < 0){
-                    X_max = X_mid;
-                    Y_Right = Y_mid;
-                }
-            }
+        if (Y_Left > Y_Mid && Y_Right > Y_Mid){
+            X_Tem = - ;
         }
+        
+        Y_Left  = (*func)(X_min);
+        Y_Mid   = (*func)(X_mid);
+        Y_Right = (*func)(X_max);
     }
 
     }
