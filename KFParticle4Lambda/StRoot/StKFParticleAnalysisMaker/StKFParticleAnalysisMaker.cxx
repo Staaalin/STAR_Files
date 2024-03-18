caec600c93b738f1aec5511ec7a6ed79f37ce093
Staaalin <943319330@qq.com>
2024-03-18 23:08:29 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -676,7 +676,7 @@ Int_t StKFParticleAnalysisMaker::Make()
 			KFParticle pv(KFParticleInterface->GetTopoReconstructor()->GetPrimVertex());
 			// tempParticle.GetDistanceToVertexLine(pv, l, dl);
 			tempParticle.SetProductionVertex(pv);
-			tempParticle.GetDecayLength(l, dl);cout<<"SCHEME 2: DecayLength = "<<l[0]<<";  ";if (fabs(v0decaylength/l[0])>1.15 || fabs(v0decaylength/l[0])<0.95){cout<<particle.GetPDG()<<"  "<<particle.GetMass()<<endl;}else{cout<<" "<<endl;}
+			tempParticle.GetDecayLength(l, dl);cout<<"SCHEME 2: DecayLength = "<<l<<";  ";if (fabs(v0decaylength/l)>1.15 || fabs(v0decaylength/l)<0.95){cout<<particle.GetPDG()<<"  "<<particle.GetMass()<<endl;}else{cout<<" "<<endl;}
 			QA_Decay_Length.emplace_back(v0decaylength);QA_DCA_V0_PV.emplace_back(dcav0toPV);
 			if (particle.GetPDG() == OmegaPdg ) { OmegaVec.push_back(particle);Omega_Omegab_Num ++;}
 			if (particle.GetPDG() == -1*OmegaPdg ) {Omega_Omegab_Num ++;}
