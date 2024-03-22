f7499c3ac057949e4919f92f85d3604f6edd5331
Staaalin <943319330@qq.com>
2024-03-22 11:42:06 +0800

    Update HADDr.C

--- RootFile/HADDr.C
+++ RootFile/HADDr.C
@@ -66,9 +66,10 @@ void HADDr()
 
 //////////////////////////////////////////////////////////////////////////////
     
-    TH1F* Result,Temp;
+    // TH1F* Result,Temp;
     TString midname = "/star/data01/pwg/svianping/output/output_";
 
+    TList *list = new TList;
     int Itr = 0;
     for(int i=65690;i <= 66389;i++){
         TString filename = midname;
@@ -78,19 +79,23 @@ void HADDr()
         cout<<"FUCK"<<endl;
         TFile *fileR = TFile::Open(filename);
         TH1F* h1 = (TH1F*)fileR->Get("h_Good_Mass");
-        if (Itr == 0) {
-            Result = (TH1F*)h1->Clone();
-        }
-        else{
-            Temp = (TH1F*)h1->Clone();
-            Result->Add(Result,Temp,1.0,1.0);
-        }
-        cout<<Itr<<" : "<<Result->Integral(0,5)<<endl;
+        list->Add(h1);
+        // if (Itr == 0) {
+        //     Result = (TH1F*)h1->Clone();
+        // }
+        // else{
+        //     Temp = (TH1F*)h1->Clone();
+        //     Result->Add(Result,Temp,1.0,1.0);
+        // }
+        // cout<<Itr<<" : "<<Result->Integral(1,1000)<<endl;
         fileR->Close();
         Itr++;
     }
+    TH1F *Result = (TH1F*)h1->Clone("h");
+    Result->Reset();
+    Result->Merge(list);
     TFile *file = new TFile("HADDr.root", "RECREATE");
-    cout<<Result->Integral(0,5)<<endl;
+    cout<<Result->Integral(0,1000)<<endl;
     Result->Write("h_Good_Mass");
     // file->Write();
     file->Close();
