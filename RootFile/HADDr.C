fa8dcd7f56e82fe23ccbd64379c6d8a3c626d1b2
Staaalin <943319330@qq.com>
2024-03-22 11:34:42 +0800

    Update HADDr.C

--- RootFile/HADDr.C
+++ RootFile/HADDr.C
@@ -83,7 +83,7 @@ void HADDr()
         }
         else{
             Temp = (TH1F*)h1->Clone();
-            Add(Result,Temp,1.0,1.0);
+            Result->Add(Result,Temp,1.0,1.0);
         }
         cout<<Itr<<" : "<<Result->Integral(0,5)<<endl;
         fileR->Close();
