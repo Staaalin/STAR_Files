226f4042d39c8192a2724fdcbcc0ff4f34d87c9e
Staaalin <943319330@qq.com>
2024-04-15 22:21:25 +0800

    UPCODE

---
+++ RootFile/HADD_Fit.csh
@@ -0,0 +1,6 @@
+#!/bin/csh
+
+set InputName = "~/Result/HADDrR_0.root"
+set OutputName = "~/Result/cHADDrR_0.root"
+
+root4star -q -b HADDr_Fit\($InputName,$OutputName\)
--- RootFile/HADDr_Fit.C
+++ RootFile/HADDr_Fit.C
@@ -32,35 +32,90 @@
 #include <stdio.h>
 using namespace std;
 
+const int PolyI = 10;
+
 Double_t FitFuction(Double_t *x, Double_t *params){
     // params[0]  多项式常数项
     // params[i]  多项式级数项
     // params[10] 高斯分布幅值
     // params[11] 高斯分布\mu
+    // params[12] 高斯分布\sigma
 
     Double_t value = params[0];
-    for (Int_t i = 1; i < 10; ++i) {
+    for (Int_t i = 1; i < PolyI; ++i) {
        value += params[i] * TMath::Power(x[0], i); // 多项式的求和
     }
-    value += params[10]*TMath::Exp((x[0] - params[11]));
+    value += params[PolyI]*TMath::Exp(-1.0*TMath::Power((x[0] - params[PolyI+1]),2)/(2*params[PolyI+2]*params[PolyI+2]));
     return value;
 }
 
-void HADDr(const TString InputName,const TString OutputName)
+int XValue2BinID(TH1F* histogram, double x){
+    int binCount = histogram->GetNbinsX();
+    for (int i = 0;i<binCount;i++){
+        if (x <= histogram->GetBinContent(i)){
+            return i;
+        }
+    }
+}
+
+double calculateAverage(TH1F* histogram, int startBin, int endBin) {
+   // 求和变量和计数器
+   double sum = 0;
+   int count = 0;
+
+   // 遍历区间内的bin，并求和
+   for (int bin = startBin; bin <= endBin; bin++) {
+      double binContent = histogram->GetBinContent(bin);
+      sum += binContent;
+      count++;
+   }
+
+   // 计算平均值
+   double average = sum / count;
+
+   return average;
+}
+
+void HADDr_Fit(const TString InputName,const TString OutputName)
 {
     TString List_Name[4] = { "HM_Lambda" , "HM_Lambdab" , "HM_Omega" , "HM_Omegab"};
     TString TCan_Name[4] = { "CM_Lambda" , "CM_Lambdab" , "CM_Omega" , "CM_Omegab"};
+    double  FIT_X_Min[4] = {    1.09     ,    1.09      ,   1.63     ,    1.63    };
+    double  FIT_X_Max[4] = {    1.14     ,    1.14      ,   1.71     ,    1.71    };
+    double  FIT_X_Wid[4] = {    0.005    ,    0.005     ,   0.009    ,    0.009   };
+    double  FIT_X_Mid[4] = {    1.1165   ,    1.1165    ,   1.6725   ,    1.6725  };
+    double  FIT_A_Min[4] = {    1.09     ,    1.09      ,   1.635    ,    1.635   };
+    double  FIT_A_Max[4] = {    1.10     ,    1.10      ,   1.663    ,    1.663   };
     TH1F* h[4];
     TCanvas *canvas[4];
     TFile *fileR = TFile::Open(InputName,"read");
+    cout<<"#####################################################################################"<<endl;
+    TFile *file = new TFile(OutputName, "RECREATE");
     for (int i=0;i<4;i++) {
+        cout<<"Start in "<<List_Name[i]<<endl;
         h[i] = (TH1F*)fileR->Get(List_Name[i]);
         canvas[i] = new TCanvas(TCan_Name[i] , TCan_Name[i]);
+        int maxBinIndex = h[i]->GetMaximumBin();
+        double maxBinValue = h[i]->GetBinContent(maxBinIndex);
+        cout<<"MaxBin at "<<maxBinIndex<<", and value is "<<maxBinValue<<endl;
+        TF1 *customFunction = new TF1("SFunction", FitFuction, FIT_X_Min[i], FIT_X_Max[i], PolyI+3);
+        double OrderFCT1 = calculateAverage(h[i] , XValue2BinID(h[i], FIT_A_Min[i]) , XValue2BinID(h[i], FIT_A_Max[i]));
+        cout<<"The 1st order poly factor is "<<OrderFCT1<<endl;
+        if       (PolyI == 10) {
+            customFunction->SetParameters(OrderFCT1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, maxBinValue - OrderFCT1, FIT_X_Mid[i] , 0.5*FIT_X_Wid[i]);
+        }else if (PolyI == 5)  {
+            customFunction->SetParameters(OrderFCT1, 0.0, 0.1, 0.2, 0.3, maxBinValue - OrderFCT1, FIT_X_Mid[i] , 0.5*FIT_X_Wid[i]);
+        }
         h[i]->Draw();
+        h[i]->Fit(customFunction);
+        customFunction->Draw("same");
+        canvas[i]->Draw();
+        cout<<"#####################################################################################"<<endl;
     }
-    TFile *file = new TFile(OutputName, "RECREATE");
+    
 
     file->Close();
+    fileR->Close();
 
 
     return;
