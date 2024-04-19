51bf32c16635ff39bd2c365997965c21291de488
Staaalin <943319330@qq.com>
2024-04-20 00:20:13 +0800

    UPCODE

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -34,8 +34,10 @@
 
 #include "StTrackHelix.h"
 #include "StLambdaDecayPair.h"
+#include "TriggerList.h"
 #include "MyToolkit.h"
 
+#define DataName           "dAu_200_16"
 #define pi                 TMath::Pi()
 #define OmegaPdgMass	   1.67245
 //#define OmegaMassSigma     0.0021
@@ -497,38 +499,48 @@ Int_t StKFParticleAnalysisMaker::Make()
 	// 		&&(!mEvent->isTrigger(610041))
 	// 		&&(!mEvent->isTrigger(610051))
 	// )return kStOK; 
-	if ((!mEvent->isTrigger(530003))
-	  &&(!mEvent->isTrigger(530002))
-	  &&(!mEvent->isTrigger(530806))
-	  &&(!mEvent->isTrigger(530101))
-	  &&(!mEvent->isTrigger(530102))
-	  &&(!mEvent->isTrigger(530201))
-	  &&(!mEvent->isTrigger(530202))
-	  &&(!mEvent->isTrigger(530213))
-	  &&(!mEvent->isTrigger(530851))// bbc
-	  &&(!mEvent->isTrigger(530852))// zdce
-	  &&(!mEvent->isTrigger(530853))// vpd-10
-	  &&(!mEvent->isTrigger(530854))// vpd
-	  &&(!mEvent->isTrigger(530855))// zdc
-	  &&(!mEvent->isTrigger(530856))// bbcnotac
-	  &&(!mEvent->isTrigger(530857))// zdc-notac
-	  &&(!mEvent->isTrigger(530861))// bbc
-	  &&(!mEvent->isTrigger(530866))// bbcnotac
-	  &&(!mEvent->isTrigger(2)) //17132063
-	  &&(!mEvent->isTrigger(3))
-	  &&(!mEvent->isTrigger(4)) //VPD-5
-	  &&(!mEvent->isTrigger(6)) //highMult-VPD-5
-	  &&(!mEvent->isTrigger(15))
-	  &&(!mEvent->isTrigger(16)) //BHT2-VPD-30
-	  &&(!mEvent->isTrigger(17))
-	  &&(!mEvent->isTrigger(54))
-	  &&(!mEvent->isTrigger(55))
-	  &&(!mEvent->isTrigger(56))
-	  &&(!mEvent->isTrigger(57))
-	  &&(!mEvent->isTrigger(58))
-	  &&(!mEvent->isTrigger(59))
-	  &&(!mEvent->isTrigger(61)) //vpd-30
-	  )return kStOK;
+	// if ((!mEvent->isTrigger(530003))
+	//   &&(!mEvent->isTrigger(530002))
+	//   &&(!mEvent->isTrigger(530806))
+	//   &&(!mEvent->isTrigger(530101))
+	//   &&(!mEvent->isTrigger(530102))
+	//   &&(!mEvent->isTrigger(530201))
+	//   &&(!mEvent->isTrigger(530202))
+	//   &&(!mEvent->isTrigger(530213))
+	//   &&(!mEvent->isTrigger(530851))// bbc
+	//   &&(!mEvent->isTrigger(530852))// zdce
+	//   &&(!mEvent->isTrigger(530853))// vpd-10
+	//   &&(!mEvent->isTrigger(530854))// vpd
+	//   &&(!mEvent->isTrigger(530855))// zdc
+	//   &&(!mEvent->isTrigger(530856))// bbcnotac
+	//   &&(!mEvent->isTrigger(530857))// zdc-notac
+	//   &&(!mEvent->isTrigger(530861))// bbc
+	//   &&(!mEvent->isTrigger(530866))// bbcnotac
+	//   &&(!mEvent->isTrigger(2)) //17132063
+	//   &&(!mEvent->isTrigger(3))
+	//   &&(!mEvent->isTrigger(4)) //VPD-5
+	//   &&(!mEvent->isTrigger(6)) //highMult-VPD-5
+	//   &&(!mEvent->isTrigger(15))
+	//   &&(!mEvent->isTrigger(16)) //BHT2-VPD-30
+	//   &&(!mEvent->isTrigger(17))
+	//   &&(!mEvent->isTrigger(54))
+	//   &&(!mEvent->isTrigger(55))
+	//   &&(!mEvent->isTrigger(56))
+	//   &&(!mEvent->isTrigger(57))
+	//   &&(!mEvent->isTrigger(58))
+	//   &&(!mEvent->isTrigger(59))
+	//   &&(!mEvent->isTrigger(61)) //vpd-30
+	//   )return kStOK;
+	int TriggerListLength = TriggerList[DataName].size();
+	bool IfTriggerMatch = false;
+	for (int i = 0;i<TriggerListLength;i++){
+		if (mEvent->isTrigger(TriggerList[DataName][i])) {
+			IfTriggerMatch = true;
+			break;
+		}
+	}
+	if (IfTriggerMatch == false) {return kStOK;}
+	
 
 	// cout<<"Trigger OK"<<endl;
 
--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/TriggerList.cpp
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/TriggerList.cpp
@@ -1,43 +1,70 @@
 #include "TriggerList.h"
 #include <cmath>
 
-std::map<TString, vector<int> > TriggerList;
-TriggerList["dAu_200_16"] = {1,
-                             2,
-                             3,
-                             4,
-                             5,
-                             }
+TriggerList["dAu_200_16"] = {
+							2,
+                            3,
+                            4,
+                            6,
+                            15,
+                            16,
+							17,
+							54,
+							55,
+							56,
+							57,
+							58,
+							59,
+							61,
+							530003,
+							530002,
+							530806,
+							530101,
+							530102,
+							530201,
+							530202,
+							530213,
+							530851,
+							530852,
+							530853,
+							530854,
+							530855,
+							530856,
+							530857,
+							530861,
+							530866
+							}
 
-	if ((!mEvent->isTrigger(530003))
-	  &&(!mEvent->isTrigger(530002))
-	  &&(!mEvent->isTrigger(530806))
-	  &&(!mEvent->isTrigger(530101))
-	  &&(!mEvent->isTrigger(530102))
-	  &&(!mEvent->isTrigger(530201))
-	  &&(!mEvent->isTrigger(530202))
-	  &&(!mEvent->isTrigger(530213))
-	  &&(!mEvent->isTrigger(530851))// bbc
-	  &&(!mEvent->isTrigger(530852))// zdce
-	  &&(!mEvent->isTrigger(530853))// vpd-10
-	  &&(!mEvent->isTrigger(530854))// vpd
-	  &&(!mEvent->isTrigger(530855))// zdc
-	  &&(!mEvent->isTrigger(530856))// bbcnotac
-	  &&(!mEvent->isTrigger(530857))// zdc-notac
-	  &&(!mEvent->isTrigger(530861))// bbc
-	  &&(!mEvent->isTrigger(530866))// bbcnotac
-	  &&(!mEvent->isTrigger(2)) //17132063
-	  &&(!mEvent->isTrigger(3))
-	  &&(!mEvent->isTrigger(4)) //VPD-5
-	  &&(!mEvent->isTrigger(6)) //highMult-VPD-5
-	  &&(!mEvent->isTrigger(15))
-	  &&(!mEvent->isTrigger(16)) //BHT2-VPD-30
-	  &&(!mEvent->isTrigger(17))
-	  &&(!mEvent->isTrigger(54))
-	  &&(!mEvent->isTrigger(55))
-	  &&(!mEvent->isTrigger(56))
-	  &&(!mEvent->isTrigger(57))
-	  &&(!mEvent->isTrigger(58))
-	  &&(!mEvent->isTrigger(59))
-	  &&(!mEvent->isTrigger(61)) //vpd-30
-	  )return kStOK;
+TriggerList["pAu_200_15"] = {
+							2,
+                            3,
+                            4,
+                            6,
+                            15,
+                            16,
+							17,
+							54,
+							55,
+							56,
+							57,
+							58,
+							59,
+							61,
+							530003,
+							530002,
+							530806,
+							530101,
+							530102,
+							530201,
+							530202,
+							530213,
+							530851,
+							530852,
+							530853,
+							530854,
+							530855,
+							530856,
+							530857,
+							530861,
+							530866
+							}
