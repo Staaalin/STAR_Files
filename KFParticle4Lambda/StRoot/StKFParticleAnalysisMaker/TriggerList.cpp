02dc50824adf0e538c4fdaccf27da72e94752006
Staaalin <943319330@qq.com>
2024-04-20 13:47:14 +0800

    Update TriggerList.cpp

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/TriggerList.cpp
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/TriggerList.cpp
@@ -5,55 +5,57 @@
 std::vector<TString> TriggerList::DataNameList;
 std::vector<std::vector<int> > TriggerList::TriggerNameList;
 
-DataNameList.clear();
-TriggerNameList.clear();
+static std::vector<TString> TriggerList::DataNameList = {"dAu_200_16",
+														 "pAu_200_15"}
 
-DataNameList.emplace_back("dAu_200_16");
-TriggerNameList.emplace_back({
-								2,
-                            	3,
-                            	4,
-                            	6,
-                            	15,
-                            	16,
-								17,
-								54,
-								55,
-								56,
-								57,
-								58,
-								59,
-								61,
-								530003,
-								530002,
-								530806,
-								530101,
-								530102,
-								530201,
-								530202,
-								530213,
-								530851,
-								530852,
-								530853,
-								530854,
-								530855,
-								530856,
-								530857,
-								530861,
-								530866
-								});
+static std::vector<std::vector<int>> TriggerList::TriggerNameList = {
 
-DataNameList.emplace_back("pAu_200_15");
-TriggerNameList.emplace_back({
-								-1,
-								-10,
-								});
+	{// dAu_200_16
+		2,
+		3,
+		4,
+		6,
+		15,
+		16,
+		17,
+		54,
+		55,
+		56,
+		57,
+		58,
+		59,
+		61,
+		530003,
+		530002,
+		530806,
+		530101,
+		530102,
+		530201,
+		530202,
+		530213,
+		530851,
+		530852,
+		530853,
+		530854,
+		530855,
+		530856,
+		530857,
+		530861,
+		530866
+	},
+
+	{// pAu_200_15
+		-2,
+		-3
+	}
+
+}
 
 std::vector<int> TriggerList::GetTriggerList()
 {
 	for (int i=0;i<DataNameList.size();i++){
-		if (DataNameList[i] == DataName){
-			std::vector<int> result = TriggerNameList[DataName];
+		if (DataNameList[i] == TriggerList::DataName){
+			std::vector<int> result = TriggerNameList[i];
 			return result;
 		}
 	}
