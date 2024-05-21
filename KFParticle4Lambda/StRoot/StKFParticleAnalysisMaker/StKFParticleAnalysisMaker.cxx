55358ec9935e131d8ff13206ff5609696a2cc9c8
Staaalin <943319330@qq.com>
2024-05-21 10:29:57 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -778,9 +778,14 @@ Int_t StKFParticleAnalysisMaker::Make()
 		KFParticle particle = KFParticleInterface->GetParticles()[iKFParticle];
 		if (particle.GetPDG() == -1){continue;}
 		if ( (fabs(particle.GetPDG()) == OmegaPdg) || (fabs(particle.GetPDG()) == XiPdg)  || (fabs(particle.GetPDG()) == LambdaPdg) ) {
+			cout<<"###############################################"<<endl;
+			cout<<"iKFParticle = "<<iKFParticle<<endl;
+			cout<<"particle.GetPDG() = "<<particle.GetPDG()<<endl;
 			for (int iDaughter=0; iDaughter < particle.NDaughters(); iDaughter++){
 				const int daughterId = particle.DaughterIds()[iDaughter];
 				const KFParticle daughter = KFParticleInterface->GetParticles()[daughterId];
+				cout<<"daughterId = "<<daughterId<<endl;
+				cout<<"daughter.GetPDG() = "<<daughter.GetPDG()<<endl;
 				if (daughter.GetPDG() != -1) {
 					DaughterParticle.push_back(daughterId);
 					MatherPartiecle.push_back(iKFParticle);
@@ -790,10 +795,25 @@ Int_t StKFParticleAnalysisMaker::Make()
 						const int GdaughterId = daughter.DaughterIds()[jDaughter];
 						DaughterParticle.push_back(GdaughterId);
 						MatherPartiecle.push_back(iKFParticle);
+						cout<<"GrandDaughterId = "<<GdaughterId<<endl;
+						cout<<"GrandDaughter.GetPDG() = "<<(KFParticleInterface->GetParticles()[GdaughterId]).GetPDG()<<endl;
 					}
 				}
 			}
+			cout<<"MatherPartiecle = ["<<endl;
+			for (int Itr = 0;Itr < DaughterParticle.size();Itr++) {
+				cout<<" "<<MatherPartiecle[Itr]<<" "<<endl;
+				if (Itr == DaughterParticle.size()-1) {cout<<"]"<<endl;}
+				else{cout<<",";}
+			}
+			cout<<"DaughterParticle = ["<<endl;
+			for (int Itr = 0;Itr < DaughterParticle.size();Itr++) {
+				cout<<" "<<DaughterParticle[Itr]<<" "<<endl;
+				if (Itr == DaughterParticle.size()-1) {cout<<"]"<<endl;}
+				else{cout<<",";}
+			}
 		}
+		DaughterParticle.resize(0);MatherPartiecle.resize(0);
 	}
 	for (int Itr = 0;Itr < DaughterParticle.size();Itr++){
 		for (int Jtr = Itr;Jtr < DaughterParticle.size();Jtr++){
