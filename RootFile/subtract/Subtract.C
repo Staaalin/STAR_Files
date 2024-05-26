a7cd6b481f521258247454f7253cfdfe31b2a4f0
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-05-26 22:29:42 +0800

    Update Subtract.C

--- RootFile/subtract/Subtract.C
+++ RootFile/subtract/Subtract.C
@@ -40,10 +40,156 @@ void Subtract(TString MidName,int StartFileIndex,int EndFileIndex,int OutputFile
                    float XiBarFitMass,float XiBarFitMassSigma,
                    float OmegaBarFitMass,float OmegaBarFitMassSigma)
 {
+
+    #if ROOT_VERSION_CODE >= ROOT_VERSION(6,0,0) 
+        std::vector<Int_t>   *id              = nullptr;
+        std::vector<Int_t>   *evtID           = nullptr;
+        std::vector<Int_t>   *runID           = nullptr;
+        std::vector<Float_t> *px              = nullptr;
+        std::vector<Float_t> *py              = nullptr;
+        std::vector<Float_t> *pz              = nullptr;
+        std::vector<Float_t> *mass            = nullptr;
+        std::vector<Float_t> *energy          = nullptr;
+        
+        TBranch *bid              = nullptr;
+        TBranch *bevtID           = nullptr;
+        TBranch *brunID           = nullptr;
+        TBranch *bpx              = nullptr;
+        TBranch *bpy              = nullptr;
+        TBranch *bpz              = nullptr;
+        TBranch *bmass            = nullptr;
+        TBranch *benergy          = nullptr;
+
+        std::vector<Float_t> *dEdx               = nullptr;
+        std::vector<Float_t> *m2                 = nullptr;
+        std::vector<Float_t> *dcatopv            = nullptr;
+        std::vector<int>     *hasTOF             = nullptr;
+        std::vector<Float_t> *nSigmaProton       = nullptr;
+        std::vector<Float_t> *nSigmaPion         = nullptr;
+        std::vector<Float_t> *nSigmaKaon         = nullptr;
+        std::vector<double>  *zTOF_proton        = nullptr;
+        std::vector<double>  *zTOF_pion          = nullptr;
+        std::vector<double>  *zTOF_kaon          = nullptr;
+	    std::vector<int>     *IfConfuse          = nullptr;
+        std::vector<double>  *Decay_Length       = nullptr;
+	    std::vector<int>     *IfBadReconstructed = nullptr;
+
+        TBranch *bdEdx               = nullptr;
+        TBranch *bm2                 = nullptr;
+        TBranch *bdcatopv            = nullptr;
+        TBranch *bhasTOF             = nullptr;
+        TBranch *bnSigmaProton       = nullptr;
+        TBranch *bnSigmaPion         = nullptr;
+        TBranch *bnSigmaKaon         = nullptr;
+        TBranch *bzTOF_proton        = nullptr;
+        TBranch *bzTOF_pion          = nullptr;
+        TBranch *bzTOF_kaon          = nullptr;
+	    TBranch *bIfConfuse          = nullptr;
+	    TBranch *bDecay_Length       = nullptr;
+	    TBranch *bIfBadReconstructed = nullptr;
+    
+    #else
+        #if ROOT_VERSION_CODE >= ROOT_VERSION(5,0,0)
+            std::vector<Int_t>   *id              = NULL;
+            std::vector<Int_t>   *evtID           = NULL;
+            std::vector<Int_t>   *runID           = NULL;
+            std::vector<Float_t> *px              = NULL;
+            std::vector<Float_t> *py              = NULL;
+            std::vector<Float_t> *pz              = NULL;
+            std::vector<Float_t> *mass            = NULL;
+            std::vector<Float_t> *energy          = NULL;
+            
+            TBranch *bid              = NULL;
+            TBranch *bevtID           = NULL;
+            TBranch *brunID           = NULL;
+            TBranch *bpx              = NULL;
+            TBranch *bpy              = NULL;
+            TBranch *bpz              = NULL;
+            TBranch *bmass            = NULL;
+            TBranch *benergy          = NULL;
+        
+            std::vector<Float_t> *dEdx               = NULL;
+            std::vector<Float_t> *m2                 = NULL;
+            std::vector<Float_t> *dcatopv            = NULL;
+            std::vector<int>     *hasTOF             = NULL;
+            std::vector<Float_t> *nSigmaProton       = NULL;
+            std::vector<Float_t> *nSigmaPion         = NULL;
+            std::vector<Float_t> *nSigmaKaon         = NULL;
+            std::vector<double>  *zTOF_proton        = NULL;
+            std::vector<double>  *zTOF_pion          = NULL;
+            std::vector<double>  *zTOF_kaon          = NULL;
+            std::vector<int>     *IfConfuse          = NULL;
+            std::vector<double>  *Decay_Length       = NULL;
+	        std::vector<int>     *IfBadReconstructed = NULL;
+
+            TBranch *bdEdx               = NULL;
+            TBranch *bm2                 = NULL;
+            TBranch *bdcatopv            = NULL;
+            TBranch *bhasTOF             = NULL;
+            TBranch *bnSigmaProton       = NULL;
+            TBranch *bnSigmaPion         = NULL;
+            TBranch *bnSigmaKaon         = NULL;
+            TBranch *bzTOF_proton        = NULL;
+            TBranch *bzTOF_pion          = NULL;
+            TBranch *bzTOF_kaon          = NULL;
+            TBranch *bIfConfuse          = NULL;
+            TBranch *bDecay_Length       = NULL;
+	        TBranch *bIfBadReconstructed = NULL;
+
+        #else
+            std::vector<Int_t>   *id              = 0;
+            std::vector<Int_t>   *evtID           = 0;
+            std::vector<Int_t>   *runID           = 0;
+            std::vector<Float_t> *px              = 0;
+            std::vector<Float_t> *py              = 0;
+            std::vector<Float_t> *pz              = 0;
+            std::vector<Float_t> *mass            = 0;
+            std::vector<Float_t> *energy          = 0;
+            
+            TBranch *bid              = 0;
+            TBranch *bevtID           = 0;
+            TBranch *brunID           = 0;
+            TBranch *bpx              = 0;
+            TBranch *bpy              = 0;
+            TBranch *bpz              = 0;
+            TBranch *bmass            = 0;
+            TBranch *benergy          = 0;
+
+            std::vector<Float_t> *dEdx               = 0;
+            std::vector<Float_t> *m2                 = 0;
+            std::vector<Float_t> *dcatopv            = 0;
+            std::vector<int>     *hasTOF             = 0;
+            std::vector<Float_t> *nSigmaProton       = 0;
+            std::vector<Float_t> *nSigmaPion         = 0;
+            std::vector<Float_t> *nSigmaKaon         = 0;
+            std::vector<double>  *zTOF_proton        = 0;
+            std::vector<double>  *zTOF_pion          = 0;
+            std::vector<double>  *zTOF_kaon          = 0;
+            std::vector<int>     *IfConfuse          = 0;
+            std::vector<double>  *Decay_Length       = 0;
+	        std::vector<int>     *IfBadReconstructed = 0;
+
+            TBranch *bdEdx               = 0;
+            TBranch *bm2                 = 0;
+            TBranch *bdcatopv            = 0;
+            TBranch *bhasTOF             = 0;
+            TBranch *bnSigmaProton       = 0;
+            TBranch *bnSigmaPion         = 0;
+            TBranch *bnSigmaKaon         = 0;
+            TBranch *bzTOF_proton        = 0;
+            TBranch *bzTOF_pion          = 0;
+            TBranch *bzTOF_kaon          = 0;
+            TBranch *bIfConfuse          = 0;
+            TBranch *bDecay_Length       = 0;
+	        TBranch *bIfBadReconstructed = 0;
+
+        #endif
+    #endif
+
     // TString FileName = "output_";
     cout<<"Start Running"<<endl;
-    cout<<StartFileIndex<<endl;
-    cout<<EndFileIndex<<endl;
+    // cout<<StartFileIndex<<endl;
+    // cout<<EndFileIndex<<endl;
 
     map<int, float> massList;
     map<int, float> massListSigma;
@@ -83,20 +229,6 @@ void Subtract(TString MidName,int StartFileIndex,int EndFileIndex,int OutputFile
 
     massList.insert(pair<int, float>(3112, 1.19745));//Sigma^-
     massList.insert(pair<int, float>(-3112, 1.19745));
-
-
-    const Int_t maxMultiplicity = 20000;
-    Int_t mult,npart;
-    Int_t id[maxMultiplicity];
-    Float_t px[maxMultiplicity],py[maxMultiplicity],pz[maxMultiplicity];
-    Float_t nSigmaPion[maxMultiplicity],nSigmaKaon[maxMultiplicity],nSigmaProton[maxMultiplicity],mass[maxMultiplicity];
-
-    // const Int_t maxMultiplicity = 20000;
-    // Int_t mult,npart;
-    // Int_t id;
-    // Float_t px,py,pz;
-    // Float_t nSigmaPion,nSigmaKaon,nSigmaProton,mass;
-
 
     #define HyperonPhaseNum  2
     #define MesonPhaseNum    2
@@ -295,15 +427,17 @@ void Subtract(TString MidName,int StartFileIndex,int EndFileIndex,int OutputFile
         // cout<<"Add "<<filename<<" Successfully"<<endl;
     }
     
+    Int_t mult,npart;
+
     hadronTree->SetBranchAddress("PDGMult",&mult);
-    hadronTree->SetBranchAddress("PDG",id);
-    hadronTree->SetBranchAddress("mix_px",px);
-    hadronTree->SetBranchAddress("mix_py",py);
-    hadronTree->SetBranchAddress("mix_pz",pz);
-    hadronTree->SetBranchAddress("InvariantMass",mass);
-    hadronTree->SetBranchAddress("nSigmaProton",nSigmaProton);
-    hadronTree->SetBranchAddress("nSigmaPion",nSigmaPion);
-    hadronTree->SetBranchAddress("nSigmaKaon",nSigmaKaon);
+    hadronTree->SetBranchAddress("PDG",&id,&bid);
+    hadronTree->SetBranchAddress("mix_px",&px,&bpx);
+    hadronTree->SetBranchAddress("mix_py",&py,&bpy);
+    hadronTree->SetBranchAddress("mix_pz",&pz,&bpz);
+    hadronTree->SetBranchAddress("InvariantMass",&mass,&bmass);
+    hadronTree->SetBranchAddress("nSigmaProton",&nSigmaProton,&bnSigmaProton);
+    hadronTree->SetBranchAddress("nSigmaPion"  ,&nSigmaPion  ,&bnSigmaPion  );
+    hadronTree->SetBranchAddress("nSigmaKaon"  ,&nSigmaKaon  ,&bnSigmaKaon  );
 
     const Int_t nentries=hadronTree->GetEntries();
     cout << "file number: " << nentries << endl;
