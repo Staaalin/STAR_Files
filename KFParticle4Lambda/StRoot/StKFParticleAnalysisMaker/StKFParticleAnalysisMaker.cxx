761f0507781287bd89581a6cf1d5aca58558687f
Staaalin <943319330@qq.com>
2024-06-11 20:39:12 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -1078,9 +1078,9 @@ Int_t StKFParticleAnalysisMaker::Make()
 
 	const double DVz = VertexZ-vpdVz;
 
-	if(fabs(VertexZ) > 8) return kStOK; // AuAu27 80 ; dAu@39 25
+	if(fabs(VertexZ) > 80) return kStOK; // AuAu27 80 ; dAu@39 25
 	if(sqrt(pow(VertexX,2.)+pow(VertexY,2.))>2.0) return kStOK; 
-	if(fabs(VertexZ-vpdVz)>3.) return kStOK;       // no vpd cut in low energy?
+	// if(fabs(VertexZ-vpdVz)>3.) return kStOK;       // no vpd cut in low energy?
 
 	//check run number
 	int runnumberPointer = -999;
