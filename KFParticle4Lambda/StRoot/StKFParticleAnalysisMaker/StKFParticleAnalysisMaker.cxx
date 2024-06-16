04cc18322a225d706ecb394b016939ed3836ce40
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-06-16 22:32:59 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -1109,8 +1109,9 @@ Int_t StKFParticleAnalysisMaker::Make()
 	if(mRefMultCorr->isBadRun(runID)) return kStOK; // reject bad run of StRefMultCorr
 	if(!mRefMultCorr->passnTofMatchRefmultCut(1.*refMult, 1.*tofMatch)) return kStOK; // reject pileup of StRefMultCorr
 
+	float DiffDVZCenter = -1.51381;// d+Au@200GeV RUN 16
 	// if (!(6<fabs(vpdVz-VertexZ) && fabs(vpdVz-VertexZ)<30 && fabs(vpdVz)<20)) return kStOK; // band test
-	if (!(fabs(vpdVz-VertexZ)<3 && 20<fabs(vpdVz) && fabs(vpdVz)<35)) return kStOK; // center test
+	if (!(fabs(vpdVz-VertexZ)<3 && 20<fabs(vpdVz-DiffDVZCenter) && fabs(vpdVz-DiffDVZCenter)<35)) return kStOK; // center test
 
 	hVertex2D ->Fill(VertexZ,vpdVz);
 	hDiffVz   ->Fill(VertexZ-vpdVz); 
