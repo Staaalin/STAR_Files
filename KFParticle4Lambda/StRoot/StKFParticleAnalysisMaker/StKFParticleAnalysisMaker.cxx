fac7efdd6475c27c28ac2f80ea06d9a4b1e62c89
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-06-27 17:06:41 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -40,10 +40,11 @@
 // #define DataName           "pAu_200_15"
 // #define DataName           "AuAu_27_18"
 // #define DataName           "dAu_200_16"
-#define DataName           "dAu_200_21"
+// #define DataName           "dAu_200_21"
 // #define DataName           "dAu_62_16"
 // #define DataName           "dAu_39_16"
 // #define DataName           "dAu_20_16"
+#define DataName           "pp_200_15"
 #define pi                 TMath::Pi()
 #define OmegaPdgMass	   1.67245
 #define XiPdgMass	       1.1972
