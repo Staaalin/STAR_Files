a004764055e6ca8b2567bc49fd03e822f563bd79
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-07-21 15:52:21 +0800

    Update MixEvent.C

--- RootFile/subtract/MixEvent.C
+++ RootFile/subtract/MixEvent.C
@@ -379,7 +379,7 @@ void MixEvent(TString MidName,int StartFileIndex,int EndFileIndex,int OutputFile
             }
 
             float tEnergy = pow(pow(B_Px[0],2) + pow(B_Py[0],2) + pow(B_Pz[0],2) + pow(massList(B_PDG),2),0.5);
-            rap = 0.5*log((tEnergy+B_Pz[0])/(tEnergy-B_Pz[0]));
+            rap = 0.5*log((tEnergy+B_Pz[0])/(tEnergy-B_Pz[0]));cout<<"rap = "<<rap<<endl;
             int RapIndex = -1;
             for (int k=0;k<yBinNum;k++){
                 if ((yBin[k] <= rap) && (rap < yBin[k+1])) {
@@ -398,7 +398,7 @@ void MixEvent(TString MidName,int StartFileIndex,int EndFileIndex,int OutputFile
             }
 
             if ((CenIndex == -1) || (RapIndex == -1) || (PtIndex == -1)) {
-                cout<<"CenIndex = "<<CenIndex<<" , "<<"RapIndex = "<<RapIndex<<"PtIndex = "<<PtIndex<<endl;
+                cout<<"CenIndex = "<<CenIndex<<" , "<<"RapIndex = "<<" , "<<RapIndex<<"PtIndex = "<<PtIndex<<endl;
                 continue;
             }
 
