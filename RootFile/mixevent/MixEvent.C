51b632e78a6758973df90aeac2e1d8d89992bcc2
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-08-25 17:18:07 +0800

    Update MixEvent.C

--- RootFile/mixevent/MixEvent.C
+++ RootFile/mixevent/MixEvent.C
@@ -557,7 +557,8 @@ void MixEvent(TString MidName,int StartFileIndex,int EndFileIndex,int OutputFile
             }
         }
 
-        cout<<"A_Px.size() = "<<A_Px.size()<<" , B_Px.size() = "<<B_Px.size()<<endl;
+        if ((A_Px.size() != 0) || (B_Px.size() != 0)) cout<<"A_Px.size() = "<<A_Px.size()<<" , B_Px.size() = "<<B_Px.size()<<endl;
+        
         if ((A_Px.size() == 0) || (B_Px.size() == 0)) continue;
         //                  mid   sid      used as binary bool
         int A_Pattern[] = {  0  ,  0  };
