b31b1ad4a139c06e5eda8d88c679c7da0adfe776
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-09-04 23:43:32 +0800

    Update MixEvent_Vector.C

--- RootFile/mixevent/MixEvent_Vector.C
+++ RootFile/mixevent/MixEvent_Vector.C
@@ -509,7 +509,7 @@ void MixEvent_Vector(TString MidName,int StartFileIndex,int EndFileIndex,int Out
         }
         cout<<"4"<<endl;
 
-        if ((A_Px.size() == 0) || (B_Px.size() == 0)) {continue;}
+        if ((A_Px.size() == 0) || (B_Px.size() == 0)) {cout<<"yes"<<endl;continue;}
         cout<<"3"<<endl;
 
         for (int Bid = 0;Bid < B_Px.size();Bid++) {
