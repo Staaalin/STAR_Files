9951bd38e3425f1c47d8d7071042c324b04a5198
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-10-13 15:39:03 +0800

    Update TPCandTOF.cpp

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/TPCandTOF.cpp
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/TPCandTOF.cpp
@@ -24,6 +24,9 @@ float TPCandTOF::KaonTPCCenter(float Pt , TString DataName)
 		else if ( 0.6 <= Pt && Pt < 0.7) {return -0.08;}
 		else                             {return 0.0  ;}
 	}
+    else{
+        return 0.0;
+    }
 }
 
 std::vector<float> TPCandTOF::KaonTOFm2(float Pt, TString DataName)
@@ -43,6 +46,26 @@ std::vector<float> TPCandTOF::KaonTOFm2(float Pt, TString DataName)
         else if (1.2 <= Pt && Pt < 1.3) { result = {0.131, 0.326}; }
         else if (1.3 <= Pt && Pt < 1.4) { result = {0.115, 0.335}; }
         else                            { result = {0.115, 0.335}; }
+        return result;
     }
-    return result;
+    else if (DataName == "dAu_200_16") { // tbd
+        if      (0.2 <= Pt && Pt < 0.3) { result = {0.228, 0.261}; }
+        else if (0.3 <= Pt && Pt < 0.4) { result = {0.227, 0.259}; }
+        else if (0.4 <= Pt && Pt < 0.5) { result = {0.224, 0.261}; }
+        else if (0.5 <= Pt && Pt < 0.6) { result = {0.219, 0.264}; }
+        else if (0.6 <= Pt && Pt < 0.7) { result = {0.213, 0.268}; }
+        else if (0.7 <= Pt && Pt < 0.8) { result = {0.206, 0.273}; }
+        else if (0.8 <= Pt && Pt < 0.9) { result = {0.196, 0.282}; }
+        else if (0.9 <= Pt && Pt < 1.0) { result = {0.185, 0.290}; }
+        else if (1.0 <= Pt && Pt < 1.1) { result = {0.172, 0.299}; }
+        else if (1.1 <= Pt && Pt < 1.2) { result = {0.152, 0.313}; }
+        else if (1.2 <= Pt && Pt < 1.3) { result = {0.131, 0.326}; }
+        else if (1.3 <= Pt && Pt < 1.4) { result = {0.115, 0.335}; }
+        else                            { result = {0.115, 0.335}; }
+        return result;
+    }
+    else{
+        result = {0.115, 0.335};
+        return result;
+    }
 }
