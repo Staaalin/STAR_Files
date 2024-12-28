6e576c83ea3ca2f95f1e1018075561906f9dbf09
Staaalin <134143724+Staaalin@users.noreply.github.com>
2024-12-28 23:39:15 +0800

    Create NC.C

---
+++ RootFile/NC.C
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <dirent.h>
+// #include <random>
+#include "math.h"
+#include "string.h"
+#include <vector>
+// #ifndef __CINT__
+#include "TROOT.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TChain.h"
+#include "TF1.h"
+#include "TH1.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TTree.h"
+#include "TNtuple.h"
+#include "TRandom.h"
+#include "TMath.h"
+#include "TVector3.h"
+#include "TLorentzVector.h"
+#include "TSystem.h"
+#include "TLegend.h"
+#include "TUnixSystem.h"
+#include "TRandom3.h"
+// #endif
+#include <iostream>
+#include <map>
+#include <stdio.h>
+using namespace std;
+
+
+#define A_Num_Per_Event 5
+#define B_Num_Per_Event 5
+#define Max_Event_Per_Pool 100 // no larger than 255
+
+struct Particle{
+    unsigned short int TreeID;
+    float Px;
+    float Py;
+    float Pz;
+
+    float Rap;
+    float Pt;
+};
+
+struct ParticlePool{
+    unsigned int EvtID;
+    bool IfMadeSame = false;
+    unsigned short int ListA_Index;
+    Particle ListA[A_Num_Per_Event];
+    unsigned short int ListB_Index;
+    Particle ListB[B_Num_Per_Event];
+};
+
+void f(float (&MassAndKstar)[2]) {
+    MassAndKstar[0] = 5.0;
+    MassAndKstar[1] = 10.0;
+}
+
+void NC()
+{
+	float R[2] = {0,1};
+    f(R);
+    cout<<"R = ["<<R[0]<<","<<R[1]<<"]"<<endl;
+
+	return;
+}
