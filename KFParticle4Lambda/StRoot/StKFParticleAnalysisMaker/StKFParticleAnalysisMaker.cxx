19e93ec9d63163669c4fd825357fb218cadc52f0
Staaalin <134143724+Staaalin@users.noreply.github.com>
2025-01-05 00:01:14 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -1301,7 +1301,7 @@ Int_t StKFParticleAnalysisMaker::Make()
 	const int tofMatch  = mEvent->nBTOFMatch();
 
 	const double magnet = mEvent->bField();
-	// cout<<"magnet = "<<magnet<<endl;as
+	cout<<"magnet = "<<magnet<<endl;
 
 	// int SizeOf_Recorded_runID = Recorded_runID.size();
 	// if (SizeOf_Recorded_runID == 0){
