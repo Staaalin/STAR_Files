43383c92c03428ea44d596838ce575d66f396d4b
Staaalin <134143724+Staaalin@users.noreply.github.com>
2025-04-12 19:35:55 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -3218,7 +3218,7 @@ Int_t StKFParticleAnalysisMaker::Make()
 				Particle_A = ChangeMass(KFParticleInterface->GetParticles()[KFParticleList[iKFParticle][1]]);
 				Particle_B = ChangeMass(KFParticleInterface->GetParticles()[KFParticleList[jKFParticle][1]]);
 				Particle_N2[0] = &Particle_A;Particle_N2[1] = &Particle_B;
-				Particle_M.Construct(Particle_N2, 2, KFP_PV_P);
+				Particle_M.Construct(Particle_N2, 2, KFP_PV_P, 2.012);
 				H_OmegaR_XiK_Mass->Fill(Particle_M.GetMass());
 			}
 		}
@@ -3291,6 +3291,9 @@ Int_t StKFParticleAnalysisMaker::Make()
 					Particle_N3[0] = &Particle_A;Particle_N3[1] = &Particle_B;Particle_N3[2] = &Particle_C;
 					Particle_M.Construct(Particle_N3, 3, KFP_PV_P);
 					H_OmegaR_OmegaPiPi_Mass->Fill(Particle_M.GetMass());
+					cout<<"Particle_A Mass = "<<Particle_A.GetMass()<<" , PDG = "<<Particle_A.GetPDG()<<endl;
+					cout<<"Particle_B Mass = "<<Particle_B.GetMass()<<" , PDG = "<<Particle_B.GetPDG()<<endl;
+					cout<<"Particle_C Mass = "<<Particle_C.GetMass()<<" , PDG = "<<Particle_C.GetPDG()<<endl;
 				}
 			}
 		}
