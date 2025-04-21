13e0d802c2a05dfdafec8ba25b36aaafc046365e
Staaalin <134143724+Staaalin@users.noreply.github.com>
2025-04-21 20:47:37 +0800

    up

---
+++ QA-Group/PSY/RunCD.C
@@ -0,0 +1,28 @@
+
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TString.h"
+
+// #include "/star/u/svianping/STAR_Files/QA-Group/PSY/StRoot/StPicoEvent/StPicoDst.h"
+
+class StRefMultCorr;
+class CentralityMaker;
+
+//_________________
+void RunCD(int File_Index , const Char_t *inFile = "test.list") {
+  // Next line is not needed if you are not running in a standalone mode
+//  gROOT->ProcessLine("#define _VANILLA_ROOT_");
+  gROOT->Macro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+  gSystem->Load("StPicoEvent");
+  gSystem->Load("StEpdUtil");
+  gSystem->Load("StRefMultCorr");
+  TString str;
+  str = ".x CD.C++(";
+  // str = ".x Gamma_QA_Simple.C++(";
+  str += File_Index;
+  str += ",\"";
+  str += inFile;
+  str += "\")";
+//  cout<<str.Data()<<endl;
+  gROOT->ProcessLine( str.Data() );
+}
