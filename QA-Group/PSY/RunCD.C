0407626c6ddcc6c55485b6e4b62be1cb4a82d1bc
Staaalin <134143724+Staaalin@users.noreply.github.com>
2025-06-01 22:22:37 +0800

    UP

--- QA-Group/PSY/RunCD.C
+++ QA-Group/PSY/RunCD.C
@@ -12,9 +12,8 @@ class CentralityMaker;
 void RunCD(const Char_t *inFile = "test.list") {
   // Next line is not needed if you are not running in a standalone mode
 //  gROOT->ProcessLine("#define _VANILLA_ROOT_");
-  // gROOT->Macro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
-  // gSystem->Load("StPicoEvent");
-  gSystem->Load("StRoot/StPicoEvent");
+  gROOT->Macro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+  gSystem->Load("StPicoEvent");
   // gSystem->Load("StEpdUtil");
   // gSystem->Load("StRefMultCorr");
   TString str;
