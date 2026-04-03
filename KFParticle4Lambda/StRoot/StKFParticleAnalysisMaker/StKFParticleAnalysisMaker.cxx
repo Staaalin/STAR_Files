e8dd6c2ed5b7c6e7ec35db5059994ebb26354456
Staaalin <943319330@qq.com>
2026-04-03 18:43:53 +0800

    Update StKFParticleAnalysisMaker.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -1402,7 +1402,8 @@ Int_t StKFParticleAnalysisMaker::Make()
 	bool IfRecorded_B_Matched = false;
 	bool IfRecordThisEventInTree = false;
 	////////////////////////////////////////////////////////
-	int Recorded_Particle[] = { KaonPdg , ProtonPdg , PionPdg , K0SPdg , LambdaPdg , XiPdg , OmegaPdg , XiRPdg}; // Record which particle
+	// int Recorded_Particle[] = { KaonPdg , ProtonPdg , PionPdg , K0SPdg , LambdaPdg , XiPdg , OmegaPdg , XiRPdg}; // Record which particle
+	int Recorded_Particle[] = { KaonPdg , LambdaPdg , XiPdg , OmegaPdg}; // Record which particle
 	int Recorded_Particle_Size = sizeof(Recorded_Particle)/sizeof(Recorded_Particle[0]);
 	StPicoEvent* mEvent= (StPicoEvent*) mPicoDst->event(); 
 	if(!mEvent)return kStOK;
