f3c3b3ac30df01c5783d8710aa6c6e573186a5d1
Staaalin <943319330@qq.com>
2026-04-03 08:57:31 +0800

    Update StKFParticleInterface.cxx

--- KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleInterface.cxx
+++ KFParticle4Lambda/StRoot/StKFParticleAnalysisMaker/StKFParticleInterface.cxx
@@ -14,6 +14,7 @@
 #include "StPicoEvent/StPicoEvent.h"
 #include "StPicoEvent/StPicoTrack.h"
 #include "StPicoEvent/StPicoBTofPidTraits.h"
+#include "StPicoEvent/StPicoTrackCovMatrix.h"
 
 #include "StBichsel/Bichsel.h"
 #include "StBichsel/StdEdxModel.h"
