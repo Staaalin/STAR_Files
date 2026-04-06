a1acba3d254c8414f5709ad8a1b44a5585d6600a
Staaalin <943319330@qq.com>
2026-04-06 16:08:06 +0800

    Update MM.cpp

--- RootFile/mixevent/MM.cpp
+++ RootFile/mixevent/MM.cpp
@@ -35,6 +35,9 @@
 #include <stdio.h>
 using namespace std;
 
+// 使用这个编译：
+// singularity exec -e --env DISPLAY=$DISPLAY -B /direct -B /gpfs -B /star -B /cvmfs -B /sdcc/lustre02 /cvmfs/star.sdcc.bnl.gov/containers/rhic_sl7.sif csh
+// g++ -O2 -std=c++11 MM.cpp -o MM `root-config --cflags --libs`
 
 // 定义粒子结构体
 struct ArmParticle {
