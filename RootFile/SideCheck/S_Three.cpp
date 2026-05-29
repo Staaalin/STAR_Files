4e69262037d43313675fe7834d67dc5037b22253
Staaalin <943319330@qq.com>
2026-05-29 19:39:41 +0800

    Update S_Three.cpp

--- RootFile/SideCheck/S_Three.cpp
+++ RootFile/SideCheck/S_Three.cpp
@@ -1013,6 +1013,7 @@ void S_Three(
                                 const auto& B_particles = eventB.B_particles;
                         
                                 for (int Cid = 0; Cid < HowMuchEventMixing + 1; ++Cid) {
+                                    if ((Check_B_C) && (Cid < Bid)) continue;
                         
                                     auto& eventC = EventPool[CenIndex][RapIndex][PVzIndex][Cid];
                                     const auto& C_particles = eventC.C_particles;
@@ -1032,7 +1033,7 @@ void S_Three(
                                     else if ((Aid != Bid) && (Aid == Cid)) {
                                         mixType = AC_B;
                                     }
-                                    else if ((Cid != Bid) && (Aid == Cid)) {
+                                    else if ((Cid == Bid) && (Aid != Cid)) {
                                         mixType = BC_A;
                                     }
                                     else{
@@ -1093,7 +1094,7 @@ void S_Three(
                             
                                             for (k=0;k<C_particles.size();k++) {
 
-                                                if (Check_B_C) {
+                                                if ((Check_B_C) && (mixType = SAME)) {
                                                     if (k <= j) continue;
                                                     if (IfInVector(C_particles[k].TreeID , B.ParentID)) continue;
                                                 }
